818ff7b711f66eb55b9f449a719c19cdff89b043
thaodd <thaott2009@gmail.com>
2024-08-24 00:54:29 +0700

    Update form template

--- Client/src/app/backend/dashboard/dashboard-routing.module.ts
+++ Client/src/app/backend/dashboard/dashboard-routing.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 import { AuthGuardService } from 'src/app/lib/services/guard.service';
-import { AppLayoutComponent } from 'src/app/layout/app.layout.component';
 
 @NgModule({
     imports: [RouterModule.forChild([
             { path: '', component: DashboardComponent },
-            { path: 'certificate', loadChildren: () => import('../ACertificate/ACertificate.module').then(m => m.ACertificateModule) }
+            { path: 'certificate', canActivate: [AuthGuardService], loadChildren: () => import('../ACertificate/ACertificate.module').then(m => m.ACertificateModule) }
         ])],
     exports: [RouterModule]
 })
